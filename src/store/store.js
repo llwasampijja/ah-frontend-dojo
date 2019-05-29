8edb7da7086fa394c8cc18642668aa7ccb10deee
Innocent Lou <bisonlou@gmail.com>
2019-05-29 10:40:21 +0300

    Chore (Setup): Setup redux (#8)
    * install redux, react-redux, and redux-thunk
    * create an appropriate folder structure for working with redux
    
    [Finishes #165273518]

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './rootReducer';
+
+const enhancers = composeWithDevTools(
+  applyMiddleware(thunk),
+);
+const store = createStore(reducer, {}, enhancers);
+
+export default store;
